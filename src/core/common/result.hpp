ef67ea5abfeb1e9e55ba137522fa064938697504
Albin Johansson <albinoscar99@hotmail.com>
2023-02-20 21:12:23 +0100

    Create result.hpp

---
+++ src/core/common/result.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace gravel {
+
+/// Simple type that encodes to possible values: success or failure.
+/// Intended to be used to communicate non-fatal errors in void functions.
+class Result final {
+ public:
+  [[nodiscard]] constexpr static auto make_success() noexcept -> Result
+  {
+    return Result {true};
+  }
+
+  [[nodiscard]] constexpr static auto make_failure() noexcept -> Result
+  {
+    return Result {false};
+  }
+
+  [[nodiscard]] constexpr auto succeeded() const noexcept { return mSucceeded; }
+
+  [[nodiscard]] constexpr auto failed() const noexcept { return !mSucceeded; }
+
+ private:
+  bool mSucceeded {};
+
+  constexpr explicit Result(const bool success) noexcept
+      : mSucceeded {success}
+  {
+  }
+};
+
+inline constexpr Result kSuccess = Result::make_success();
+inline constexpr Result kFailure = Result::make_failure();
+
+}  // namespace gravel
