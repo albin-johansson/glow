ff8d14c766df168298a558fabfa05f8860d4470d
Albin Johansson <albinoscar99@hotmail.com>
2023-02-20 21:40:56 +0100

    Create string.hpp

---
+++ src/core/common/type/string.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>       // string
+#include <string_view>  // string_view
+
+namespace gravel {
+
+using String = std::string;
+using StringView = std::string_view;
+
+}  // namespace gravel
