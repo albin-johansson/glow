4206255ca10cc5d4c71e88a94fd2a14204f0288e
Albin Johansson <albinoscar99@hotmail.com>
2023-02-21 23:14:19 +0100

    Add OpenGL vertex array class

---
+++ src/core/graphics/opengl/vertex_array.cpp
@@ -0,0 +1,77 @@
+#include "vertex_array.hpp"
+
+#include <cassert>  // assert
+
+#include <glad/glad.h>
+
+#include "graphics/opengl/util.hpp"
+#include "util/bits.hpp"
+
+namespace gravel::gl {
+
+VertexArray::VertexArray()
+{
+  glGenVertexArrays(1, &mID);
+}
+
+VertexArray::~VertexArray() noexcept
+{
+  dispose();
+}
+
+VertexArray::VertexArray(VertexArray&& other) noexcept
+    : mID {other.mID}
+{
+  other.mID = 0;
+}
+
+VertexArray& VertexArray::operator=(VertexArray&& other) noexcept
+{
+  if (this != &other) {
+    dispose();
+
+    mID = other.mID;
+    other.mID = 0;
+  }
+
+  return *this;
+}
+
+void VertexArray::dispose() noexcept
+{
+  if (mID != 0) {
+    glDeleteVertexArrays(1, &mID);
+  }
+}
+
+void VertexArray::bind()
+{
+  glBindVertexArray(mID);
+}
+
+void VertexArray::unbind()
+{
+  glBindVertexArray(0);
+}
+
+void VertexArray::init_attr(const uint location,
+                            const int value_count,
+                            const uint value_type,
+                            const usize vertex_size,
+                            const usize offset,
+                            const bool normalized)
+{
+  assert(get_bound_vertex_array() == mID);
+
+  glVertexAttribPointer(location,
+                        value_count,
+                        value_type,
+                        normalized ? GL_TRUE : GL_FALSE,
+                        static_cast<int>(vertex_size),
+                        bitcast<void*>(offset));
+  glEnableVertexAttribArray(location);
+
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+}  // namespace gravel::gl
---
+++ src/core/graphics/opengl/vertex_array.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "common/predef.hpp"
+#include "common/primitives.hpp"
+
+namespace gravel::gl {
+
+/// Represents an OpenGL vertex array object (VAO).
+class VertexArray final {
+ public:
+  GRAVEL_DELETE_COPY(VertexArray);
+
+  /// Creates a VAO instance, but does not bind it.
+  VertexArray();
+
+  ~VertexArray() noexcept;
+
+  VertexArray(VertexArray&& other) noexcept;
+
+  auto operator=(VertexArray&& other) noexcept -> VertexArray&;
+
+  /// Binds the VAO for subsequent draw calls.
+  void bind();
+
+  /// Unbinds any bound VAO.
+  static void unbind();
+
+  /**
+   * Initializes a vertex attribute slot and enables it.
+   *
+   * \pre The VAO must be bound when this function is called.
+   *
+   * \param location the vertex attribute index.
+   * \param value_count the amount of values in the attribute, e.g. '3' for 3D vectors.
+   * \param value_type the primitive type stored in the attribute, e.g. 'GL_FLOAT'.
+   * \param vertex_size the total size of a vertex including all attributes.
+   * \param offset the offset of the attribute.
+   * \param normalized controls whether the values are normalized.
+   */
+  void init_attr(uint location,
+                 int value_count,
+                 uint value_type,
+                 usize vertex_size,
+                 usize offset = 0,
+                 bool normalized = false);
+
+  /// Returns the OpenGL identifier associated with the VAO.
+  [[nodiscard]] auto get_id() const -> uint { return mID; }
+
+ private:
+  uint mID {};
+
+  void dispose() noexcept;
+};
+
+}  // namespace gravel::gl
