9006123a2de971b1d00f7cd00444bbf3e78c9535
Albin Johansson <albinoscar99@hotmail.com>
2023-02-22 00:00:19 +0100

    Add simple OpenGL program class

---
+++ src/core/graphics/opengl/program.cpp
@@ -0,0 +1,243 @@
+#include "program.hpp"
+
+#include <glad/glad.h>
+#include <glm/gtc/type_ptr.hpp>
+#include <spdlog/spdlog.h>
+
+#include "graphics/opengl/shader.hpp"
+#include "graphics/opengl/util.hpp"
+
+namespace gravel::gl {
+
+Program::Program()
+    : mID {glCreateProgram()}
+{
+}
+
+Program::~Program() noexcept
+{
+  dispose();
+}
+
+Program::Program(Program&& other) noexcept
+    : mID {other.mID}
+{
+  other.mID = 0;
+}
+
+auto Program::operator=(Program&& other) noexcept -> Program&
+{
+  if (this != &other) {
+    dispose();
+
+    mID = other.mID;
+    other.mID = 0;
+  }
+
+  return *this;
+}
+
+void Program::dispose() noexcept
+{
+  if (mID != 0) {
+    glDeleteProgram(mID);
+  }
+}
+
+auto Program::load_shader_files(const Path& vertex_path, const Path& fragment_path)
+    -> Result
+{
+  Shader vs {ShaderType::Vertex};
+  if (vs.compile_file(vertex_path).failed()) {
+    return kFailure;
+  }
+
+  Shader fs {ShaderType::Fragment};
+  if (fs.compile_file(fragment_path).failed()) {
+    return kFailure;
+  }
+
+  attach_shader(vs);
+  attach_shader(fs);
+
+  return kSuccess;
+}
+
+auto Program::load_shader_code(const char* vertex_code, const char* fragment_code)
+    -> Result
+{
+  Shader vs {ShaderType::Vertex};
+  if (vs.compile_code(vertex_code).failed()) {
+    return kFailure;
+  }
+
+  Shader fs {ShaderType::Fragment};
+  if (fs.compile_code(fragment_code).failed()) {
+    return kFailure;
+  }
+
+  attach_shader(vs);
+  attach_shader(fs);
+
+  return kSuccess;
+}
+
+void Program::attach_shader(const Shader& shader)
+{
+  glAttachShader(mID, shader.get_id());
+}
+
+auto Program::link() -> Result
+{
+  glLinkProgram(mID);
+
+  int success {};
+  glGetProgramiv(mID, GL_LINK_STATUS, &success);
+
+  if (success) {
+    return kSuccess;
+  }
+  else {
+    char msg[512] {};
+    glGetProgramInfoLog(mID, sizeof msg, nullptr, msg);
+    spdlog::error("[GL] Link error: {}", msg);
+    return kFailure;
+  }
+}
+
+void Program::bind()
+{
+  glUseProgram(mID);
+}
+
+void Program::unbind()
+{
+  glUseProgram(0);
+}
+
+void Program::set_uniform(const int location, const bool b)
+{
+  glProgramUniform1i(mID, location, b ? GL_TRUE : GL_FALSE);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform(const int location, const int i)
+{
+  glProgramUniform1i(mID, location, i);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform(const int location, const float f)
+{
+  glProgramUniform1f(mID, location, f);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform(const int location, const Vec2& vec)
+{
+  glProgramUniform2f(mID, location, vec.x, vec.y);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform(const int location, const Vec2i& vec)
+{
+  glProgramUniform2i(mID, location, vec.x, vec.y);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform(const int location, const Vec3& vec)
+{
+  glProgramUniform3f(mID, location, vec.x, vec.y, vec.z);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform(const int location, const Vec3i& vec)
+{
+  glProgramUniform3i(mID, location, vec.x, vec.y, vec.z);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform(const int location, const Vec4& vec)
+{
+  glProgramUniform4f(mID, location, vec.x, vec.y, vec.z, vec.w);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform(const int location, const Vec4i& vec)
+{
+  glProgramUniform4i(mID, location, vec.x, vec.y, vec.z, vec.w);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform(const int location, const Mat3& mat)
+{
+  glProgramUniformMatrix3fv(mID, location, 1, GL_FALSE, glm::value_ptr(mat));
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform(const int location, const Mat4& mat)
+{
+  glProgramUniformMatrix4fv(mID, location, 1, GL_FALSE, glm::value_ptr(mat));
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform_array(const int location, const ssize count, const int* array)
+{
+  glProgramUniform1iv(mID, location, static_cast<GLsizei>(count), array);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform_array(const int location, const ssize count, const float* array)
+{
+  glProgramUniform1fv(mID, location, static_cast<GLsizei>(count), array);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform_array(const int location, const ssize count, const Vec2* array)
+{
+  glProgramUniform2fv(mID, location, static_cast<GLsizei>(count), glm::value_ptr(*array));
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform_array(const int location, const ssize count, const Vec2i* array)
+{
+  glProgramUniform2iv(mID, location, static_cast<GLsizei>(count), glm::value_ptr(*array));
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform_array(const int location, const ssize count, const Vec3* array)
+{
+  glProgramUniform3fv(mID, location, static_cast<GLsizei>(count), glm::value_ptr(*array));
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform_array(const int location, const ssize count, const Vec3i* array)
+{
+  glProgramUniform3iv(mID, location, static_cast<GLsizei>(count), glm::value_ptr(*array));
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform_array(const int location, const ssize count, const Vec4* array)
+{
+  glProgramUniform4fv(mID, location, static_cast<GLsizei>(count), glm::value_ptr(*array));
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void Program::set_uniform_array(const int location, const ssize count, const Vec4i* array)
+{
+  glProgramUniform4iv(mID, location, static_cast<GLsizei>(count), glm::value_ptr(*array));
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+auto Program::get_uniform_location(const char* name) const -> Maybe<int>
+{
+  const auto index = glGetUniformBlockIndex(mID, name);
+  if (index != GL_INVALID_INDEX) {
+    return index;
+  }
+  else {
+    return kNothing;
+  }
+}
+
+}  // namespace gravel::gl
---
+++ src/core/graphics/opengl/program.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <utility>  // forward
+
+#include "common/predef.hpp"
+#include "common/primitives.hpp"
+#include "common/result.hpp"
+#include "common/type/math.hpp"
+#include "common/type/maybe.hpp"
+#include "common/type/path.hpp"
+
+namespace gravel::gl {
+
+class Shader;
+
+/// Represents an OpenGL shader program.
+class Program final {
+ public:
+  GRAVEL_DELETE_COPY(Program);
+
+  /// Creates a shader program without any attached shaders.
+  Program();
+
+  ~Program() noexcept;
+
+  Program(Program&& other) noexcept;
+
+  auto operator=(Program&& other) noexcept -> Program&;
+
+  /// Tries to load a vertex/fragment shader pair from the specified file paths.
+  auto load_shader_files(const Path& vertex_path, const Path& fragment_path) -> Result;
+
+  /// Tries to compile a vertex/fragment shader pair from the provided shader source code.
+  auto load_shader_code(const char* vertex_code, const char* fragment_code) -> Result;
+
+  /// Attaches a shader to the program.
+  void attach_shader(const Shader& shader);
+
+  /// Attempts to link the shader program, using previously attached shaders.
+  auto link() -> Result;
+
+  /// Enables the associated shaders for subsequent draw calls.
+  void bind();
+
+  /// Disables any bound shader program.
+  static void unbind();
+
+  void set_uniform(int location, bool b);
+
+  void set_uniform(int location, int i);
+
+  void set_uniform(int location, float f);
+
+  void set_uniform(int location, const Vec2& vec);
+
+  void set_uniform(int location, const Vec2i& vec);
+
+  void set_uniform(int location, const Vec3& vec);
+
+  void set_uniform(int location, const Vec3i& vec);
+
+  void set_uniform(int location, const Vec4& vec);
+
+  void set_uniform(int location, const Vec4i& vec);
+
+  void set_uniform(int location, const Mat3& mat);
+
+  void set_uniform(int location, const Mat4& mat);
+
+  template <typename T>
+  void set_uniform(const char* name, T&& value)
+  {
+    if (const auto location = get_uniform_location(name)) {
+      set_uniform(*location, std::forward<T>(value));
+    }
+  }
+
+  void set_uniform_array(int location, ssize count, const int* array);
+
+  void set_uniform_array(int location, ssize count, const float* array);
+
+  void set_uniform_array(int location, ssize count, const Vec2* array);
+
+  void set_uniform_array(int location, ssize count, const Vec2i* array);
+
+  void set_uniform_array(int location, ssize count, const Vec3* array);
+
+  void set_uniform_array(int location, ssize count, const Vec3i* array);
+
+  void set_uniform_array(int location, ssize count, const Vec4* array);
+
+  void set_uniform_array(int location, ssize count, const Vec4i* array);
+
+  template <typename T>
+  void set_uniform_array(const char* name, const ssize count, const T* array)
+  {
+    if (const auto location = get_uniform_location(name)) {
+      set_uniform_array(*location, count, array);
+    }
+  }
+
+  /// Returns the location of a uniform in the program.
+  [[nodiscard]] auto get_uniform_location(const char* name) const -> Maybe<int>;
+
+  [[nodiscard]] auto get_id() const -> uint { return mID; }
+
+ private:
+  uint mID {};
+
+  void dispose() noexcept;
+};
+
+}  // namespace gravel::gl
