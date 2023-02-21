c16be3d901d421637f4b4bedee4e4d2e8b149981
Albin Johansson <albinoscar99@hotmail.com>
2023-02-21 23:56:25 +0100

    Add OpenGL shader class

---
+++ src/core/graphics/opengl/shader.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "common/predef.hpp"
+#include "common/primitives.hpp"
+#include "common/result.hpp"
+#include "common/type/path.hpp"
+#include "graphics/shader_type.hpp"
+
+namespace gravel::gl {
+
+/**
+ * Represents an OpenGL shader object.
+ *
+ * \details Instances of this class are usually short-lived, and can be safely
+ *          discarded after being attached to a program instance.
+ */
+class Shader final {
+ public:
+  GRAVEL_DELETE_COPY(Shader);
+
+  /// Creates a shader of the specified type, without any associated code.
+  explicit Shader(ShaderType type);
+
+  ~Shader() noexcept;
+
+  Shader(Shader&& other) noexcept;
+
+  auto operator=(Shader&& other) noexcept -> Shader&;
+
+  /// Tries to compile the provided GLSL source code.
+  auto compile_code(const char* code) -> Result;
+
+  /// Tries to compile the GLSL shader file at the specified file path.
+  auto compile_file(const Path& path) -> Result;
+
+  [[nodiscard]] auto get_id() const -> uint { return mID; }
+
+ private:
+  uint mID {};
+
+  void dispose() noexcept;
+};
+
+}  // namespace gravel::gl
