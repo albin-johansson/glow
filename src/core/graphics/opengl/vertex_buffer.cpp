039ee5f921e4089964c01f2555f62ad31144968f
Albin Johansson <albinoscar99@hotmail.com>
2023-02-22 23:15:01 +0100

    Update vertex_buffer.cpp

--- src/core/graphics/opengl/vertex_buffer.cpp
+++ src/core/graphics/opengl/vertex_buffer.cpp
@@ -1,6 +1,10 @@
 #include "vertex_buffer.hpp"
 
+#include <cassert>  // assert
+
 #include <glad/glad.h>
+
+#include "graphics/opengl/util.hpp"
 
 namespace gravel::gl {
 
@@ -47,6 +51,19 @@ void VertexBuffer::bind()
 void VertexBuffer::unbind()
 {
   glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void VertexBuffer::upload_data(const usize data_size,
+                               const void* data,
+                               const BufferUsage usage)
+{
+  assert(get_bound_vertex_buffer() == mID);
+
+  glBufferData(GL_ARRAY_BUFFER,
+               static_cast<GLsizeiptr>(data_size),
+               data,
+               convert_buffer_usage(usage));
+  GRAVEL_GL_CHECK_ERRORS();
 }
 
 }  // namespace gravel::gl
