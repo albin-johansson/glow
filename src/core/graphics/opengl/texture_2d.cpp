47312a2bd1cd83f9d43679ffb7b7d9cc6e89e0be
Albin Johansson <albinoscar99@hotmail.com>
2023-02-22 22:27:49 +0100

    Add OpenGL texture class

---
+++ src/core/graphics/opengl/texture_2d.cpp
@@ -0,0 +1,85 @@
+#include "texture_2d.hpp"
+
+#include <cassert>  // assert
+
+#include <glad/glad.h>
+
+#include "graphics/opengl/util.hpp"
+
+namespace gravel::gl {
+
+Texture2D::Texture2D()
+{
+  glGenTextures(1, &mID);
+
+  bind();
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  unbind();
+
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+Texture2D::~Texture2D() noexcept
+{
+  dispose();
+}
+
+Texture2D::Texture2D(Texture2D&& other) noexcept
+    : mID {other.mID}
+{
+  other.mID = 0;
+}
+
+auto Texture2D::operator=(Texture2D&& other) noexcept -> Texture2D&
+{
+  if (this != &other) {
+    dispose();
+
+    mID = other.mID;
+    other.mID = 0;
+  }
+
+  return *this;
+}
+
+void Texture2D::dispose() noexcept
+{
+  if (mID != 0) {
+    glDeleteTextures(1, &mID);
+  }
+}
+
+void Texture2D::bind()
+{
+  glBindTexture(GL_TEXTURE_2D, mID);
+}
+
+void Texture2D::unbind()
+{
+  glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture2D::set_data(const int detail_level,
+                         const int texture_format,
+                         const uint pixel_format,
+                         const uint type,
+                         const Vec2i& size,
+                         const void* pixel_data)
+{
+  assert(get_bound_texture() == mID);
+  glTexImage2D(GL_TEXTURE_2D,
+               detail_level,
+               texture_format,
+               size.x,
+               size.y,
+               0,
+               pixel_format,
+               type,
+               pixel_data);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+}  // namespace gravel::gl
---
+++ src/core/graphics/opengl/texture_2d.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "common/predef.hpp"
+#include "common/primitives.hpp"
+#include "common/type/math.hpp"
+
+namespace gravel::gl {
+
+/// Represents a two-dimensional OpenGL texture.
+class Texture2D final {
+ public:
+  GRAVEL_DELETE_COPY(Texture2D);
+
+  /**
+   * Creates a 2D texture.
+   *
+   * \details The texture will use linear minification and magnification by default, along
+   *          with a clamp-to-edge strategy for wrapping. The texture will not be bound
+   *          when the constructor returns.
+   */
+  Texture2D();
+
+  ~Texture2D() noexcept;
+
+  Texture2D(Texture2D&& other) noexcept;
+
+  auto operator=(Texture2D&& other) noexcept -> Texture2D&;
+
+  /// Enables the texture for subsequent draw calls.
+  void bind();
+
+  /// Unbinds any bound 2D texture.
+  static void unbind();
+
+  /**
+   * Sets the pixel data associated with texture.
+   *
+   * \pre The texture must be bound when this function is called.
+   *
+   * \param detail_level the level-of-detail (LOD) index.
+   * \param texture_format  the texture format, e.g. 'GL_RGB'.
+   * \param pixel_format the format of the pixel data, e.g. 'GL_RGB'.
+   * \param type the texture format data type, e.g. 'GL_UNSIGNED_BYTE'.
+   * \param size the texture size.
+   * \param pixel_data the raw pixel data.
+   */
+  void set_data(int detail_level,
+                int texture_format,
+                uint pixel_format,
+                uint type,
+                const Vec2i& size,
+                const void* pixel_data);
+
+  [[nodiscard]] auto get_id() const -> uint { return mID; }
+
+ private:
+  uint mID {};
+
+  void dispose() noexcept;
+};
+
+}  // namespace gravel::gl
