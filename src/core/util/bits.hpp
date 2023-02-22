f5ec3ede7596dbfbd0f909b795ae0d6a9333c481
Albin Johansson <albinoscar99@hotmail.com>
2023-02-22 20:24:38 +0100

    Create bits.hpp

---
+++ src/core/util/bits.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstring>      // memcpy
+#include <type_traits>  // is_trivially_copyable_v, is_trivially_constructible_v
+
+namespace gravel {
+
+template <typename To, typename From>
+  requires(sizeof(To) == sizeof(From) &&          //
+           std::is_trivially_copyable_v<From> &&  //
+           std::is_trivially_copyable_v<To> &&    //
+           std::is_trivially_constructible_v<To>)
+[[nodiscard]] auto bitcast(const From& from) noexcept -> To
+{
+  To to;
+  std::memcpy(&to, &from, sizeof(To));
+  return to;
+}
+
+}  // namespace gravel
