b4e01a89ff5ccfaf72e36a38ac77b0b32a8d1cb2
Albin Johansson <albinoscar99@hotmail.com>
2023-02-23 00:07:24 +0100

    Add OpenGL framebuffer class

---
+++ src/core/graphics/opengl/framebuffer.cpp
@@ -0,0 +1,144 @@
+#include "framebuffer.hpp"
+
+#include <cassert>  // assert
+#include <utility>  // move
+
+#include <glad/glad.h>
+
+#include "graphics/opengl/util.hpp"
+
+namespace gravel::gl {
+
+Framebuffer::Framebuffer()
+{
+  glGenFramebuffers(1, &mID);
+  bind();
+
+  glFramebufferTexture2D(GL_FRAMEBUFFER,
+                         GL_COLOR_ATTACHMENT0,
+                         GL_TEXTURE_2D,
+                         mColorTexture.get_id(),
+                         0);
+  glFramebufferTexture2D(GL_FRAMEBUFFER,
+                         GL_DEPTH_ATTACHMENT,
+                         GL_TEXTURE_2D,
+                         mDepthTexture.get_id(),
+                         0);
+
+  glDrawBuffer(GL_COLOR_ATTACHMENT0);
+  unbind();
+
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+Framebuffer::~Framebuffer() noexcept
+{
+  dispose();
+}
+
+Framebuffer::Framebuffer(Framebuffer&& other) noexcept
+    : mID {other.mID},
+      mColorTexture {std::move(other.mColorTexture)},
+      mDepthTexture {std::move(other.mDepthTexture)},
+      mSize {other.mSize}
+{
+  other.mID = 0;
+}
+
+auto Framebuffer::operator=(Framebuffer&& other) noexcept -> Framebuffer&
+{
+  if (this != &other) {
+    dispose();
+
+    mID = other.mID;
+    mColorTexture = std::move(other.mColorTexture);
+    mDepthTexture = std::move(other.mDepthTexture);
+    mSize = other.mSize;
+
+    other.mID = 0;
+  }
+
+  return *this;
+}
+
+void Framebuffer::dispose() noexcept
+{
+  if (mID != 0) {
+    glDeleteFramebuffers(1, &mID);
+  }
+}
+
+void Framebuffer::bind()
+{
+  glBindFramebuffer(GL_FRAMEBUFFER, mID);
+}
+
+void Framebuffer::unbind()
+{
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void Framebuffer::resize(const Vec2i size)
+{
+  assert(get_bound_framebuffer() == mID);
+
+  if (mSize == size) {
+    return;
+  }
+
+  mSize = size;
+
+  mColorTexture.bind();
+  mColorTexture.set_data(0, GL_RGB, GL_RGB, GL_UNSIGNED_BYTE, mSize, nullptr);
+
+  mDepthTexture.bind();
+  mDepthTexture.set_data(0,  //
+                         GL_DEPTH_COMPONENT24,
+                         GL_DEPTH_COMPONENT,
+                         GL_FLOAT,
+                         mSize,
+                         nullptr);
+
+  Texture2D::unbind();
+}
+
+void Framebuffer::clear()
+{
+  assert(get_bound_framebuffer() == mID);
+  assert(is_complete());
+
+  glViewport(0, 0, mSize.x, mSize.y);
+
+  glClearColor(0, 0, 0, 1);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+auto Framebuffer::is_complete() const -> bool
+{
+  assert(get_bound_framebuffer() == mID);
+  return glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE;
+}
+
+auto Framebuffer::get_id() const -> uint
+{
+  return mID;
+}
+
+auto Framebuffer::get_color_texture_id() const -> uint
+{
+  return mColorTexture.get_id();
+}
+
+auto Framebuffer::get_depth_texture_id() const -> uint
+{
+  return mDepthTexture.get_id();
+}
+
+auto Framebuffer::get_size() const -> const Vec2i&
+{
+  return mSize;
+}
+
+}  // namespace gravel::gl
---
+++ src/core/graphics/opengl/framebuffer.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "common/predef.hpp"
+#include "common/primitives.hpp"
+#include "common/type/math.hpp"
+#include "graphics/opengl/texture_2d.hpp"
+
+namespace gravel::gl {
+
+/// Represents an OpenGL framebuffer object, providing a color and depth buffer.
+class Framebuffer final {
+ public:
+  GRAVEL_DELETE_COPY(Framebuffer);
+
+  /**
+   * Creates a framebuffer object (including texture objects), but does not bind it.
+   *
+   * \note You need to call the resize function to make the framebuffer usable
+   */
+  Framebuffer();
+
+  ~Framebuffer() noexcept;
+
+  Framebuffer(Framebuffer&& other) noexcept;
+
+  auto operator=(Framebuffer&& other) noexcept -> Framebuffer&;
+
+  /// Binds the framebuffer for subsequent draw calls.
+  void bind();
+
+  /// Unbinds any bound framebuffer.
+  static void unbind();
+
+  /**
+   * Sets the size of the framebuffer.
+   *
+   * \pre The framebuffer must be bound when this function is called.
+   * \param size the framebuffer size.
+   */
+  void resize(Vec2i size);
+
+  /**
+   * Sets the viewport and clears the color and depth buffers.
+   *
+   * \pre The framebuffer must be bound when this function is called.
+   * \pre The framebuffer must be complete when this function is called.
+   */
+  void clear();
+
+  [[nodiscard]] auto is_complete() const -> bool;
+
+  [[nodiscard]] auto get_id() const -> uint;
+
+  [[nodiscard]] auto get_color_texture_id() const -> uint;
+
+  [[nodiscard]] auto get_depth_texture_id() const -> uint;
+
+  [[nodiscard]] auto get_size() const -> const Vec2i&;
+
+ private:
+  uint mID {};
+  Texture2D mColorTexture;
+  Texture2D mDepthTexture;
+  Vec2i mSize {};
+
+  void dispose() noexcept;
+};
+
+}  // namespace gravel::gl
