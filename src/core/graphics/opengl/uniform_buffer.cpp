3c61ea9ab05cf76c17a5b2a86dfb97506c4a297d
Albin Johansson <albinoscar99@hotmail.com>
2023-02-26 23:41:05 +0100

    Add basic OpenGL uniform buffer class

---
+++ src/core/graphics/opengl/uniform_buffer.cpp
@@ -0,0 +1,101 @@
+#include "uniform_buffer.hpp"
+
+#include <cassert>  // assert
+#include <vector>   // vector
+
+#include <glad/glad.h>
+
+#include "graphics/opengl/util.hpp"
+
+namespace gravel::gl {
+
+UniformBuffer::UniformBuffer()
+{
+  glGenBuffers(1, &mID);
+}
+
+UniformBuffer::~UniformBuffer() noexcept
+{
+  dispose();
+}
+
+UniformBuffer::UniformBuffer(UniformBuffer&& other) noexcept
+    : mID {other.mID}
+{
+  other.mID = 0;
+}
+
+auto UniformBuffer::operator=(UniformBuffer&& other) noexcept -> UniformBuffer&
+{
+  if (this != &other) {
+    dispose();
+
+    mID = other.mID;
+    other.mID = 0;
+  }
+
+  return *this;
+}
+
+void UniformBuffer::dispose() noexcept
+{
+  if (mID != 0) {
+    glDeleteBuffers(1, &mID);
+  }
+}
+
+void UniformBuffer::bind()
+{
+  glBindBuffer(GL_UNIFORM_BUFFER, mID);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void UniformBuffer::unbind()
+{
+  glBindBuffer(GL_UNIFORM_BUFFER, 0);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void UniformBuffer::bind_block(const int binding)
+{
+  glBindBufferBase(GL_UNIFORM_BUFFER, binding, mID);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void UniformBuffer::unbind_block(const int binding)
+{
+  glBindBufferBase(GL_UNIFORM_BUFFER, binding, 0);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void UniformBuffer::reserve_space(const ssize data_size, const BufferUsage usage)
+{
+  assert(mID == get_bound_uniform_buffer());
+
+  std::vector<Byte> buffer;
+  buffer.resize(static_cast<usize>(data_size));
+
+  set_data(std::ssize(buffer), buffer.data(), usage);
+}
+
+void UniformBuffer::set_data(const ssize data_size,
+                             const void* data,
+                             const BufferUsage usage)
+{
+  assert(mID == get_bound_uniform_buffer());
+
+  glBufferData(GL_UNIFORM_BUFFER, data_size, data, convert_buffer_usage(usage));
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+void UniformBuffer::update_data(const ssize offset,
+                                const ssize data_size,
+                                const void* data)
+{
+  assert(mID == get_bound_uniform_buffer());
+
+  glBufferSubData(GL_UNIFORM_BUFFER, offset, data_size, data);
+  GRAVEL_GL_CHECK_ERRORS();
+}
+
+}  // namespace gravel::gl
---
+++ src/core/graphics/opengl/uniform_buffer.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "common/predef.hpp"
+#include "common/primitives.hpp"
+#include "graphics/buffer_usage.hpp"
+
+namespace gravel::gl {
+
+/// Represents an OpenGL uniform buffer object (UBO).
+class UniformBuffer final {
+ public:
+  GRAVEL_DELETE_COPY(UniformBuffer);
+
+  /// Creates a UBO, but does not bind it.
+  UniformBuffer();
+
+  ~UniformBuffer() noexcept;
+
+  UniformBuffer(UniformBuffer&& other) noexcept;
+
+  auto operator=(UniformBuffer&& other) noexcept -> UniformBuffer&;
+
+  /// Binds the UBO for subsequent draw calls.
+  void bind();
+
+  /// Unbinds any bound UBO.
+  static void unbind();
+
+  /// Binds the UBO to the specified binding point.
+  void bind_block(int binding);
+
+  /// Unbinds any bound UBO from the specified binding point.
+  static void unbind_block(int binding);
+
+  /**
+   * \brief Allocates the specified amount of zero-initialized storage in the UBO.
+   *
+   * \pre The UBO must be bound when this function is called.
+   *
+   * \param data_size the size of the storage, in bytes.
+   * \param usage buffer usage hint.
+   */
+  void reserve_space(ssize data_size, BufferUsage usage = BufferUsage::Dynamic);
+
+  /**
+   * Replaces the data in the UBO.
+   *
+   * \pre The UBO must be bound when this function is called.
+   *
+   * \note Frequent updates to the buffer data should be done using `update_data`.
+   *
+   * \param data_size the size of the data, in bytes.
+   * \param data pointer to the data that will be copied into the UBO.
+   * \param usage buffer usage hint.
+   */
+  void set_data(ssize data_size,
+                const void* data,
+                BufferUsage usage = BufferUsage::Dynamic);
+
+  /**
+   * Updates existing data in the UBO.
+   *
+   * \pre The UBO must be bound when this function is called.
+   *
+   * \param offset the offset of the data member that will be changed.
+   * \param data_size the size of the data member, in bytes.
+   * \param data the new data.
+   */
+  void update_data(ssize offset, ssize data_size, const void* data);
+
+ private:
+  uint mID {};
+
+  void dispose() noexcept;
+};
+
+}  // namespace gravel::gl
