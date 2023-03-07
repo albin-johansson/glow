c116eb7541bff69b21849905f74e7278a3fe840c
Albin Johansson <albinoscar99@hotmail.com>
2023-03-07 18:36:05 +0100

    Create ecs.hpp

---
+++ src/core/common/type/ecs.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <entt/entity/entity.hpp>
+#include <entt/entity/registry.hpp>
+
+namespace gravel {
+
+using Entity = entt::entity;
+using Registry = entt::registry;
+
+}  // namespace gravel
