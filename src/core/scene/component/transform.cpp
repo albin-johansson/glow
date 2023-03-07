580dbf7faecc155a4dd7e50a38ab64a5b4ab7770
Albin Johansson <albinoscar99@hotmail.com>
2023-03-07 18:35:44 +0100

    Add model matrix utility

---
+++ src/core/scene/component/transform.cpp
@@ -0,0 +1,20 @@
+#include "transform.hpp"
+
+#include <glm/gtx/transform.hpp>
+
+namespace gravel::comp {
+
+auto Transform::to_model_matrix() const -> Mat4
+{
+  Mat4 matrix {1};
+
+  matrix = glm::translate(matrix, translation);
+  matrix = glm::rotate(matrix, glm::radians(rotation.x), Vec3 {1, 0, 0});
+  matrix = glm::rotate(matrix, glm::radians(rotation.y), Vec3 {0, 1, 0});
+  matrix = glm::rotate(matrix, glm::radians(rotation.z), Vec3 {0, 0, 1});
+  matrix = glm::scale(matrix, scale);
+
+  return matrix;
+}
+
+}  // namespace gravel::comp
--- src/core/scene/component/transform.hpp
+++ src/core/scene/component/transform.hpp
@@ -8,6 +8,8 @@ struct Transform final {
   Vec3 translation {0, 0, 0};
   Vec3 rotation {0, 0, 0};
   Vec3 scale {1, 1, 1};
+
+  [[nodiscard]] auto to_model_matrix() const -> Mat4;
 };
 
 }  // namespace gravel::comp
