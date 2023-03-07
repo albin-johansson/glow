53429ed6e05703ed90047c8a464858b071abd36f
Albin Johansson <albinoscar99@hotmail.com>
2023-03-07 17:39:07 +0100

    Make vertices hashable and comparable

--- src/core/graphics/vertex.hpp
+++ src/core/graphics/vertex.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <cstddef>     // size_t
+#include <functional>  // hash
+
+#include <glm/gtx/hash.hpp>
+
+#include "common/hash.hpp"
 #include "common/type/math.hpp"
 
 namespace gravel {
@@ -8,6 +14,16 @@ struct Vertex final {
   Vec3 position {};
   Vec3 normal {};
   Vec2 tex_coords {};
+
+  [[nodiscard]] auto operator==(const Vertex&) const -> bool = default;
 };
 
 }  // namespace gravel
+
+template <>
+struct std::hash<gravel::Vertex> final {
+  [[nodiscard]] auto operator()(const gravel::Vertex& vertex) const -> std::size_t
+  {
+    return gravel::hash_combine(vertex.position, vertex.normal, vertex.tex_coords);
+  }
+};
