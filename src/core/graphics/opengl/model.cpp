18f4970ee0ff76c7b272630b257e71e7f4ce8307
Albin Johansson <albinoscar99@hotmail.com>
2023-03-14 16:09:16 +0100

    Add texture cache component

--- src/core/graphics/opengl/model.cpp
+++ src/core/graphics/opengl/model.cpp
@@ -1,27 +1,51 @@
 #include "model.hpp"
 
 #include <cstdlib>  // offsetof
+#include <utility>  // move
 
 #include <glad/glad.h>
 
+#include "common/type/map.hpp"
 #include "common/type/vector.hpp"
+#include "graphics/opengl/texture_cache.hpp"
 #include "io/model_loader.hpp"
+#include "io/texture_loader.hpp"
 
 namespace gravel::gl {
 namespace {
 
-[[nodiscard]] auto create_material(Registry& registry, const MaterialData& material_data)
-    -> Entity
+[[nodiscard]] auto create_texture(TextureCache& cache, Path path) -> Maybe<uint>
 {
+  if (const auto iter = cache.textures.find(path); iter != cache.textures.end()) {
+    return iter->second.get_id();
+  }
+
+  if (auto texture = Texture2D::load_rgb(path)) {
+    const auto texture_id = texture->get_id();
+    cache.textures.try_emplace(std::move(path), std::move(*texture));
+    return texture_id;
+  }
+
+  return kNothing;
+}
+
+[[nodiscard]] auto create_material(Registry& registry,
+                                   const MaterialData& material_data,
+                                   const Path& model_dir) -> Entity
+{
+  auto& texture_cache = registry.ctx().get<TextureCache>();
+
   const auto material_entity = registry.create();
   auto& material = registry.emplace<Material>(material_entity);
 
   if (material_data.diffuse_tex.has_value()) {
-    // TODO
+    material.diffuse_tex =
+        create_texture(texture_cache, model_dir / *material_data.diffuse_tex);
   }
 
   if (material_data.specular_tex.has_value()) {
-    // TODO
+    material.specular_tex =
+        create_texture(texture_cache, model_dir / *material_data.specular_tex);
   }
 
   material.ambient = material_data.ambient;
@@ -74,7 +98,8 @@ void assign_model(Registry& registry, const Entity entity, const Path& path)
     material_entities.reserve(model_data->materials.size());
 
     for (const auto& [material_id, material_data] : model_data->materials) {
-      const auto material_entity = create_material(registry, material_data);
+      const auto material_entity =
+          create_material(registry, material_data, model_data->dir);
       material_entities[material_id] = material_entity;
     }
 
---
+++ src/core/graphics/opengl/texture_cache.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "common/predef.hpp"
+#include "common/type/map.hpp"
+#include "common/type/path.hpp"
+#include "graphics/opengl/texture_2d.hpp"
+
+namespace gravel::gl {
+
+/// Context component used to keep track of loaded textures.
+struct TextureCache final {
+  TextureCache() = default;
+  ~TextureCache() = default;
+  GRAVEL_DELETE_COPY(TextureCache);
+  GRAVEL_DEFAULT_MOVE(TextureCache);
+
+  Map<Path, Texture2D> textures;
+};
+
+}  // namespace gravel::gl
