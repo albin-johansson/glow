ceabcab2aa5856ad6dc95acb68c9e88b186266cd
Albin Johansson <albinoscar99@hotmail.com>
2023-03-18 15:13:49 +0100

    Add smart pointer aliases

---
+++ src/core/common/type/memory.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <memory>  // unique_ptr, shared_ptr, weak_ptr, default_delete
+
+namespace gravel {
+
+template <typename T, typename Deleter = std::default_delete<T>>
+using Unique = std::unique_ptr<T, Deleter>;
+
+template <typename T>
+using Shared = std::shared_ptr<T>;
+
+template <typename T>
+using Weak = std::weak_ptr<T>;
+
+}  // namespace gravel
