3bfbf64d1729d7be319ce0a9cd2d34de1b8122e2
Albin Johansson <albinoscar99@hotmail.com>
2023-03-24 14:42:42 +0100

    Add Vulkan surface class

---
+++ src/core/graphics/vulkan/surface.cpp
@@ -0,0 +1,23 @@
+#include "surface.hpp"
+
+#include <SDL2/SDL_vulkan.h>
+#include <fmt/format.h>
+
+#include "common/debug/error.hpp"
+
+namespace gravel::vk {
+
+Surface::Surface(SDL_Window* window, VkInstance instance)
+    : mInstance {instance}
+{
+  if (!SDL_Vulkan_CreateSurface(window, mInstance, &mSurface)) {
+    throw Error {fmt::format("[VK] Could not create Vulkan surface: {}", SDL_GetError())};
+  }
+}
+
+Surface::~Surface()
+{
+  vkDestroySurfaceKHR(mInstance, mSurface, nullptr);
+}
+
+}  // namespace gravel::vk
---
+++ src/core/graphics/vulkan/surface.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <volk.h>
+
+#include "common/predef.hpp"
+
+namespace gravel::vk {
+
+/// RAII wrapper for `VkSurfaceKHR`.
+class Surface final {
+ public:
+  GRAVEL_DELETE_COPY(Surface);
+  GRAVEL_DELETE_MOVE(Surface);
+
+  Surface(SDL_Window* window, VkInstance instance);
+
+  ~Surface();
+
+  [[nodiscard]] auto get() -> VkSurfaceKHR { return mSurface; }
+
+ private:
+  VkInstance mInstance {};
+  VkSurfaceKHR mSurface {};
+};
+
+}  // namespace gravel::vk
