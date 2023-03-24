1a5021681b8c04621362aaf1ee947507bfeb1bad
Albin Johansson <albinoscar99@hotmail.com>
2023-03-24 14:24:38 +0100

    Add set aliases

---
+++ src/core/common/type/set.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <functional>     // less
+#include <set>            // set
+#include <unordered_set>  // unordered_set
+
+namespace gravel {
+
+template <typename T>
+using Set = std::set<T, std::less<>>;
+
+template <typename T>
+using HashSet = std::unordered_set<T>;
+
+}  // namespace gravel
