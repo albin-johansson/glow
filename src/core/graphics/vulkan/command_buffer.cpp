5c81723a29bbaf737633942df519bd458beb23ec
Albin Johansson <albinoscar99@hotmail.com>
2023-03-26 18:12:52 +0200

    Add command pool and buffer factory functions

--- src/core/graphics/vulkan/command_buffer.cpp
+++ src/core/graphics/vulkan/command_buffer.cpp
@@ -1,8 +1,45 @@
 #include "command_buffer.hpp"
 
+#include "graphics/vulkan/physical_device.hpp"
 #include "graphics/vulkan/util.hpp"
 
 namespace gravel::vlk {
+
+auto create_command_pool(VkDevice device, VkPhysicalDevice gpu, VkSurfaceKHR surface)
+    -> VkCommandPool
+{
+  const auto queue_family_indices = get_queue_family_indices(gpu, surface);
+
+  const VkCommandPoolCreateInfo create_info {
+      .sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+      .pNext = nullptr,
+      .flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
+      .queueFamilyIndex = queue_family_indices.graphics_family.value(),
+  };
+
+  VkCommandPool command_pool {VK_NULL_HANDLE};
+  GRAVEL_VK_CALL(vkCreateCommandPool(device, &create_info, nullptr, &command_pool),
+                 "[VK] Could not create command pool");
+
+  return command_pool;
+}
+
+auto create_command_buffer(VkDevice device, VkCommandPool command_pool) -> VkCommandBuffer
+{
+  const VkCommandBufferAllocateInfo allocate_info {
+      .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+      .pNext = nullptr,
+      .commandPool = command_pool,
+      .level = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+      .commandBufferCount = 1,
+  };
+
+  VkCommandBuffer command_buffer {VK_NULL_HANDLE};
+  GRAVEL_VK_CALL(vkAllocateCommandBuffers(device, &allocate_info, &command_buffer),
+                 "[VK] Could not create command buffer");
+
+  return command_buffer;
+}
 
 void reset_command_buffer(VkCommandBuffer command_buffer)
 {
--- src/core/graphics/vulkan/command_buffer.hpp
+++ src/core/graphics/vulkan/command_buffer.hpp
@@ -4,6 +4,13 @@
 
 namespace gravel::vlk {
 
+[[nodiscard]] auto create_command_pool(VkDevice device,
+                                       VkPhysicalDevice gpu,
+                                       VkSurfaceKHR surface) -> VkCommandPool;
+
+[[nodiscard]] auto create_command_buffer(VkDevice device, VkCommandPool command_pool)
+    -> VkCommandBuffer;
+
 void reset_command_buffer(VkCommandBuffer command_buffer);
 
 void begin_command_buffer(VkCommandBuffer command_buffer);
