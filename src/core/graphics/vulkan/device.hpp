a258e12643a89203719040e3d34e1177a3a643fb
Albin Johansson <albinoscar99@hotmail.com>
2023-03-26 17:41:10 +0200

    Add submit/present functions to device class

--- src/core/graphics/vulkan/device.hpp
+++ src/core/graphics/vulkan/device.hpp
@@ -3,6 +3,7 @@
 #include <vulkan/vulkan.h>
 
 #include "common/predef.hpp"
+#include "common/primitives.hpp"
 
 namespace gravel::vlk {
 
@@ -14,6 +15,17 @@ class Device final {
   Device(VkPhysicalDevice gpu, VkSurfaceKHR surface);
 
   ~Device();
+
+  /// Submits rendering commands to the graphics queue.
+  void submit_rendering_commands(VkCommandBuffer command_buffer,
+                                 VkSemaphore image_available_semaphore,
+                                 VkSemaphore render_finished_semaphore,
+                                 VkFence in_flight_fence);
+
+  /// Presents a swapchain image to the presentation queue.
+  void present_swapchain_image(VkSwapchainKHR swapchain,
+                               uint32 swapchain_image_index,
+                               VkSemaphore render_finished_semaphore);
 
   [[nodiscard]] auto get() -> VkDevice { return mDevice; }
   [[nodiscard]] auto get_graphics_queue() -> VkQueue { return mGraphicsQueue; }
