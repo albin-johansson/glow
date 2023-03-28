e0bfdfe940b05cb57906ed9a376baa49af21f922
Albin Johansson <albinoscar99@hotmail.com>
2023-03-29 01:13:00 +0200

    Add byte size utility function

--- src/core/common/type/vector.hpp
+++ src/core/common/type/vector.hpp
@@ -2,9 +2,17 @@
 
 #include <vector>  // vector
 
+#include "common/primitives.hpp"
+
 namespace gravel {
 
 template <typename T>
 using Vector = std::vector<T>;
+
+template <typename T>
+[[nodiscard]] auto byte_size(const Vector<T>& vector) noexcept -> usize
+{
+  return vector.size() * sizeof(T);
+}
 
 }  // namespace gravel
