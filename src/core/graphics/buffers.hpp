f405ec756c574f5e54fda4f9c1df2efd4a274659
Albin Johansson <albinoscar99@hotmail.com>
2023-03-31 14:43:13 +0200

    Explicitly align std140 buffer structs

--- src/core/graphics/buffers.hpp
+++ src/core/graphics/buffers.hpp
@@ -7,8 +7,8 @@ namespace gravel {
 
 /// This struct corresponds to a std140 layout uniform block.
 struct EnvironmentBuffer final {
-  Mat4 inverse_proj_view {};
-  Vec4 camera_pos {};
+  alignas(16) Mat4 inverse_proj_view {};
+  alignas(16) Vec4 camera_pos {};
   float32 brightness {1.0f};
   float32 gamma {2.2f};
   int32 use_gamma_correction {true};
@@ -16,18 +16,18 @@ struct EnvironmentBuffer final {
 
 /// This struct corresponds to a std140 layout uniform block.
 struct MatrixBuffer final {
-  Mat4 m {};       ///< Model matrix.
-  Mat4 mv {};      ///< Model-view matrix.
-  Mat4 mvp {};     ///< Model-view-projection matrix.
-  Mat4 normal {};  ///< Normal matrix.
+  alignas(16) Mat4 m {};       ///< Model matrix.
+  alignas(16) Mat4 mv {};      ///< Model-view matrix.
+  alignas(16) Mat4 mvp {};     ///< Model-view-projection matrix.
+  alignas(16) Mat4 normal {};  ///< Normal matrix.
 };
 
 /// This struct corresponds to a std140 layout uniform block.
 struct MaterialBuffer final {
-  Vec4 ambient {};
-  Vec4 diffuse {};
-  Vec4 specular {};
-  Vec4 emission {};
+  alignas(16) Vec4 ambient {};
+  alignas(16) Vec4 diffuse {};
+  alignas(16) Vec4 specular {};
+  alignas(16) Vec4 emission {};
   int32 has_ambient_tex {false};
   int32 has_diffuse_tex {false};
   int32 has_specular_tex {false};
