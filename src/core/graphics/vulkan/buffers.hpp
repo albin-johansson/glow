abc88db9a0f79922dabb1471aefeafb22fd11aaa
Albin Johansson <albinoscar99@hotmail.com>
2023-04-08 18:34:41 +0200

    [VK] Add buffer structs

---
+++ src/core/graphics/vulkan/buffers.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include "common/primitives.hpp"
+#include "common/type/math.hpp"
+
+namespace gravel::vlk {
+
+/// Holds matrices that remain constant over a single frame.
+/// \note this struct assumes the std140 layout!
+struct StaticMatrices final {
+  Mat4 proj {};       ///< Projection matrix.
+  Mat4 view {};       ///< View matrix.
+  Mat4 view_proj {};  ///< View-projection matrix.
+
+  [[nodiscard]] static auto descriptor_buffer_info(VkBuffer buffer)
+      -> VkDescriptorBufferInfo;
+
+  [[nodiscard]] static auto descriptor_set_write(VkDescriptorSet set,
+                                                 const VkDescriptorBufferInfo* info)
+      -> VkWriteDescriptorSet;
+};
+
+/// Intended to be used as a push constant for per model mesh matrices.
+/// \note This struct assumes the std140 layout!
+struct DynamicMatrices final {
+  Mat4 model {};       ///< Model matrix.
+  Mat4 model_view {};  ///< Model-view matrix.
+  Mat4 mvp {};         ///< Model-view-projection matrix.
+  Mat4 normal {};      ///< Normal matrix.
+
+  [[nodiscard]] static auto push_constant_range(uint32 offset, VkShaderStageFlags stages)
+      -> VkPushConstantRange;
+};
+
+}  // namespace gravel::vlk
