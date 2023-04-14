c34906d35059387972ef769058fce7083976a1ef
Albin Johansson <albinoscar99@hotmail.com>
2023-04-14 16:30:03 +0200

    Update menu_bar.cpp

--- src/core/ui/menu_bar.cpp
+++ src/core/ui/menu_bar.cpp
@@ -4,6 +4,7 @@
 #include <imgui.h>
 
 #include "graphics/environment.hpp"
+#include "graphics/renderer_info.hpp"
 #include "graphics/rendering_options.hpp"
 #include "scene/scene.hpp"
 #include "ui/events.hpp"
@@ -22,17 +23,53 @@ void show_file_menu(const Scene&, Dispatcher& dispatcher)
   }
 }
 
-void show_view_menu(const Scene& scene, Dispatcher& dispatcher)
+void show_view_menu(const Scene&, Dispatcher& dispatcher)
 {
-  const auto& rendering_options = scene.get<RenderingOptions>();
-
   if (ImGui::BeginMenu("View")) {
     if (ImGui::MenuItem(ICON_FA_ROTATE " Reset UI")) {
       dispatcher.enqueue<RestoreLayoutEvent>();
     }
 
+    if (ImGui::BeginMenu("Widgets")) {
+      if (ImGui::MenuItem("Scene", nullptr, true)) {}
+
+      if (ImGui::MenuItem("Viewport Overlay", nullptr, true)) {}
+
+      ImGui::EndMenu();
+    }
+
     ImGui::Separator();
 
+    if (ImGui::MenuItem(ICON_FA_MAGNIFYING_GLASS_PLUS " Increase Font Size")) {
+      dispatcher.enqueue<IncreaseFontSizeEvent>();
+    }
+
+    if (ImGui::MenuItem(ICON_FA_MAGNIFYING_GLASS_MINUS " Decrease Font Size")) {
+      dispatcher.enqueue<DecreaseFontSizeEvent>();
+    }
+
+    if (ImGui::MenuItem(ICON_FA_FONT " Reset Font Size")) {
+      dispatcher.enqueue<ResetFontSizeEvent>();
+    }
+
+    ImGui::Separator();
+
+    if (ImGui::MenuItem(ICON_FA_EYE " Toggle UI")) {
+      // TODO dispatcher.enqueue<ToggleUIEvent>();
+    }
+
+    ImGui::EndMenu();
+  }
+}
+
+void show_renderer_menu(const Scene& scene, Dispatcher& dispatcher)
+{
+  const auto& renderer_info = scene.get<RendererInfo>();
+  const auto& rendering_options = scene.get<RenderingOptions>();
+
+  bool show_renderer_info_popup = false;
+
+  if (ImGui::BeginMenu("Renderer")) {
     if (ImGui::MenuItem(ICON_FA_GAUGE_HIGH " VSync",
                         nullptr,
                         rendering_options.test(RenderingOption::VSync))) {
@@ -65,19 +102,25 @@ void show_view_menu(const Scene& scene, Dispatcher& dispatcher)
 
     ImGui::Separator();
 
-    if (ImGui::MenuItem(ICON_FA_MAGNIFYING_GLASS_PLUS " Increase Font Size")) {
-      dispatcher.enqueue<IncreaseFontSizeEvent>();
-    }
-
-    if (ImGui::MenuItem(ICON_FA_MAGNIFYING_GLASS_MINUS " Decrease Font Size")) {
-      dispatcher.enqueue<DecreaseFontSizeEvent>();
-    }
-
-    if (ImGui::MenuItem(ICON_FA_FONT " Reset Font Size")) {
-      dispatcher.enqueue<ResetFontSizeEvent>();
-    }
+    show_renderer_info_popup = ImGui::MenuItem("Renderer Info...");
 
     ImGui::EndMenu();
+  }
+
+  static bool popup_open = false;
+
+  if (show_renderer_info_popup) {
+    ImGui::OpenPopup("Renderer Information");
+    popup_open = true;
+  }
+
+  if (ImGui::BeginPopupModal("Renderer Information", &popup_open)) {
+    ImGui::Text("API: %s", "OpenGL 4.1.0 core");
+    ImGui::Text("Renderer: %s", renderer_info.renderer.c_str());
+    ImGui::Text("Vendor: %s", renderer_info.vendor.c_str());
+    ImGui::Text("Version: %s", renderer_info.version.c_str());
+
+    ImGui::EndPopup();
   }
 }
 
@@ -126,6 +169,13 @@ void show_environment_menu(const Scene& scene, Dispatcher& dispatcher)
   }
 }
 
+void show_help_menu(const Scene&, Dispatcher& dispatcher)
+{
+  if (ImGui::BeginMenu("Help")) {
+    ImGui::EndMenu();
+  }
+}
+
 void show_debug_menu(const Scene&, Dispatcher& dispatcher)
 {
   if (ImGui::BeginMenu("Debug")) {
@@ -150,7 +200,9 @@ void show_menu_bar(const Scene& scene, Dispatcher& dispatcher)
   if (ImGui::BeginMainMenuBar()) {
     show_file_menu(scene, dispatcher);
     show_view_menu(scene, dispatcher);
+    show_renderer_menu(scene, dispatcher);
     show_environment_menu(scene, dispatcher);
+    show_help_menu(scene, dispatcher);
     show_debug_menu(scene, dispatcher);
 
     ImGui::EndMainMenuBar();
