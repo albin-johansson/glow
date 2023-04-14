f1bf99dffa0d3b52403f1b6fcd814abf140e0d5a
Albin Johansson <albinoscar99@hotmail.com>
2023-04-14 16:24:55 +0200

    Add Roboto font

--- src/core/init/dear_imgui.cpp
+++ src/core/init/dear_imgui.cpp
@@ -39,6 +39,89 @@ void init_style(ImGuiStyle& style)
   style.WindowTitleAlign = ImVec2 {0.5f, 0.5f};
   style.WindowMenuButtonPosition = ImGuiDir_None;
   style.SeparatorTextBorderSize = 2;
+
+  const ImVec4 transparent {0.00f, 0.00f, 0.00f, 0.00f};
+  const ImVec4 dark_gray {0.02f, 0.02f, 0.02f, 1.00f};
+  const ImVec4 background {0.01f, 0.01f, 0.01f, 1.00f};
+
+  const ImVec4 primary_accent {0.00f, 0.20f, 0.25f, 1.00f};
+  const ImVec4 primary_accent_hover {0.00f, 0.30f, 0.35f, 1.0f};
+  const ImVec4 primary_accent_active {0.00f, 0.10f, 0.15f, 1.0f};
+
+  const ImVec4 secondary_accent {1.00f, 0.20f, 0.90f, 1.0f};
+  const ImVec4 secondary_accent_hover {1.00f, 0.25f, 0.95f, 1.0f};
+  const ImVec4 secondary_accent_active {0.90f, 0.15f, 0.85f, 1.0f};
+
+  style.Colors[ImGuiCol_Text] = ImVec4 {1.00f, 1.00f, 1.00f, 1.00f};
+  style.Colors[ImGuiCol_TextDisabled] = ImVec4 {0.20f, 0.20f, 0.20f, 0.80f};
+  style.Colors[ImGuiCol_TextSelectedBg] = ImVec4 {0.00f, 0.00f, 0.25f, 0.75f};
+
+  style.Colors[ImGuiCol_WindowBg] = background;
+  style.Colors[ImGuiCol_ChildBg] = transparent;
+  style.Colors[ImGuiCol_PopupBg] = background;
+  style.Colors[ImGuiCol_MenuBarBg] = dark_gray;
+
+  style.Colors[ImGuiCol_TitleBg] = primary_accent;
+  style.Colors[ImGuiCol_TitleBgCollapsed] = background;
+  style.Colors[ImGuiCol_TitleBgActive] = primary_accent;
+
+  style.Colors[ImGuiCol_FrameBg] = primary_accent;
+  style.Colors[ImGuiCol_FrameBgHovered] = primary_accent_hover;
+  style.Colors[ImGuiCol_FrameBgActive] = primary_accent_active;
+
+  style.Colors[ImGuiCol_Border] = primary_accent;
+  style.Colors[ImGuiCol_BorderShadow] = transparent;
+
+  style.Colors[ImGuiCol_CheckMark] = secondary_accent;
+
+  style.Colors[ImGuiCol_ResizeGrip] = primary_accent;
+  style.Colors[ImGuiCol_ResizeGripHovered] = primary_accent_hover;
+  style.Colors[ImGuiCol_ResizeGripActive] = primary_accent_active;
+
+  style.Colors[ImGuiCol_Separator] = primary_accent;
+  style.Colors[ImGuiCol_SeparatorHovered] = primary_accent_hover;
+  style.Colors[ImGuiCol_SeparatorActive] = primary_accent_active;
+
+  style.Colors[ImGuiCol_Header] = primary_accent;
+  style.Colors[ImGuiCol_HeaderHovered] = primary_accent_hover;
+  style.Colors[ImGuiCol_HeaderActive] = primary_accent_active;
+
+  style.Colors[ImGuiCol_Tab] = primary_accent;
+  style.Colors[ImGuiCol_TabHovered] = primary_accent_hover;
+  style.Colors[ImGuiCol_TabActive] = primary_accent_active;
+  style.Colors[ImGuiCol_TabUnfocused] = primary_accent;
+  style.Colors[ImGuiCol_TabUnfocusedActive] = primary_accent_active;
+
+  style.Colors[ImGuiCol_Button] = primary_accent;
+  style.Colors[ImGuiCol_ButtonHovered] = primary_accent_hover;
+  style.Colors[ImGuiCol_ButtonActive] = primary_accent_active;
+
+  style.Colors[ImGuiCol_SliderGrab] = secondary_accent;
+  style.Colors[ImGuiCol_SliderGrabActive] = secondary_accent_active;
+
+  style.Colors[ImGuiCol_ScrollbarBg] = transparent;
+  style.Colors[ImGuiCol_ScrollbarGrab] = primary_accent;
+  style.Colors[ImGuiCol_ScrollbarGrabHovered] = primary_accent_hover;
+  style.Colors[ImGuiCol_ScrollbarGrabActive] = primary_accent_active;
+
+  style.Colors[ImGuiCol_TableHeaderBg] = background;
+  style.Colors[ImGuiCol_TableBorderStrong] = primary_accent;
+  style.Colors[ImGuiCol_TableBorderLight] = transparent;
+
+  style.Colors[ImGuiCol_TableRowBg] = transparent;
+  style.Colors[ImGuiCol_TableRowBgAlt] = transparent;
+
+  style.Colors[ImGuiCol_PlotLines] = secondary_accent;
+  style.Colors[ImGuiCol_PlotLinesHovered] = secondary_accent_hover;
+
+  style.Colors[ImGuiCol_PlotHistogram] = secondary_accent;
+  style.Colors[ImGuiCol_PlotHistogramHovered] = secondary_accent_hover;
+
+  style.Colors[ImGuiCol_DragDropTarget] = secondary_accent;
+
+  style.Colors[ImGuiCol_NavHighlight] = secondary_accent;
+  style.Colors[ImGuiCol_NavWindowingHighlight] = secondary_accent;
+  style.Colors[ImGuiCol_NavWindowingDimBg] = transparent;
 }
 
 }  // namespace
@@ -83,20 +166,22 @@ void DearImGui::reload_fonts()
 
   ImFontConfig default_config {};
   default_config.SizePixels = mFontSize * framebuffer_scale.x;
-  io.Fonts->AddFontDefault(&default_config);
+  io.Fonts->AddFontFromFileTTF("assets/fonts/roboto/Roboto-Regular.ttf",
+                               default_config.SizePixels,
+                               &default_config);
 
   // The global scale is 1 on most platforms, and 0.5 on macOS
   io.FontGlobalScale = 1.0f / framebuffer_scale.x;
 
-  ImFontConfig fa_config {};
-  fa_config.MergeMode = true;
-  fa_config.SizePixels = mFontSize * framebuffer_scale.x;
-  fa_config.GlyphMinAdvanceX = fa_config.SizePixels;
-  fa_config.GlyphMaxAdvanceX = fa_config.GlyphMinAdvanceX;
-  fa_config.GlyphOffset = {0, 2};
+  ImFontConfig icon_config {};
+  icon_config.MergeMode = true;
+  icon_config.SizePixels = mFontSize * framebuffer_scale.x;
+  icon_config.GlyphMinAdvanceX = icon_config.SizePixels;
+  icon_config.GlyphMaxAdvanceX = icon_config.GlyphMinAdvanceX;
+  icon_config.GlyphOffset = {0, 2};
   io.Fonts->AddFontFromFileTTF("assets/fonts/fa/" FONT_ICON_FILE_NAME_FAS,
-                               fa_config.GlyphMinAdvanceX,
-                               &fa_config,
+                               icon_config.GlyphMinAdvanceX,
+                               &icon_config,
                                kFontIconRange);
 
   io.Fonts->Build();
