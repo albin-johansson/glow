d992da2511ca6edf95cdd8183ca7b0d418cd93d7
Albin Johansson <albinoscar99@hotmail.com>
2023-08-06 17:04:55 +0200

    Create render_device.hpp

---
+++ src/public/glow/core/rhi/render_device.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>  // string
+
+#include "glow/core/rhi/rhi.hpp"
+
+namespace glow {
+
+struct RenderDeviceInfo final {
+  std::string api_name;
+  std::string api_version;
+  std::string renderer_name;
+};
+
+
+/** Provides the top-level API for the render hardware interface (RHI). */
+class IRenderDevice {
+ public:
+  virtual ~IRenderDevice() noexcept = default;
+
+  [[nodiscard]] virtual auto begin_frame() -> bool = 0;
+
+  virtual void end_frame() = 0;
+
+  [[nodiscard]] virtual auto get_resource_manager() -> IResourceManager& = 0;
+  [[nodiscard]] virtual auto get_resource_manager() const -> const IResourceManager& = 0;
+
+  [[nodiscard]] virtual auto get_info() const -> RenderDeviceInfo = 0;
+};
+
+}  // namespace glow
