2b50d41aaa313fba9a6b0bb38257346a130158bd
Albin Johansson <albinoscar99@hotmail.com>
2023-08-06 16:50:38 +0200

    Create rhi.hpp

---
+++ src/public/glow/core/rhi/rhi.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "glow/core/foundation/flags.hpp"
+#include "glow/core/prelude.hpp"
+
+namespace glow {
+
+using RenderID = uint64;
+
+struct RenderDeviceInfo;
+struct RenderDeviceFeatures;
+struct VertexAttribute;
+struct VertexDeclaration;
+
+struct PipelineRasterizerState;
+struct PipelineDepthStencilState;
+struct PipelineColorBlendState;
+
+class IRenderContext;
+class IRenderDevice;
+class IResourceManager;
+class IBuffer;
+class ITexture;
+class ICommandBuffer;
+class IGraphicsPipeline;
+
+enum class Handedness : uint32;
+enum class DescriptorType : uint32;
+enum class PolygonMode : uint32;
+enum class CullMode : uint32;
+enum class PixelFormat : uint32;
+enum class ImageLayout : uint32;
+
+GLOW_FWD_FLAG_ENUM(ShaderStage, uint32);
+GLOW_FWD_FLAG_ENUM(MemoryHint, uint32);
+GLOW_FWD_FLAG_ENUM(BufferHint, uint32);
+GLOW_FWD_FLAG_ENUM(BufferUsage, uint32);
+
+}  // namespace glow
