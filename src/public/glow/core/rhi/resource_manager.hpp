7d8cca579eebdea638af812237aa3fac1f026345
Albin Johansson <albinoscar99@hotmail.com>
2023-08-06 21:46:09 +0200

    Update resource_manager.hpp

--- src/public/glow/core/rhi/resource_manager.hpp
+++ src/public/glow/core/rhi/resource_manager.hpp
@@ -5,12 +5,22 @@
 
 namespace glow {
 
-class IResourceManager {
+class GLOW_CORE_API IResourceManager {
  public:
   virtual ~IResourceManager() noexcept = default;
 
   /** Allocates, but does not initialize, a buffer resource. */
   [[nodiscard]] virtual auto allocate_buffer() -> RenderID = 0;
+
+  /** Allocates, but does not initialize, a pipeline state object. */
+  [[nodiscard]] virtual auto allocate_graphics_pipeline() -> RenderID = 0;
+
+  [[nodiscard]] virtual auto get_buffer(RenderID id) -> IBuffer& = 0;
+  [[nodiscard]] virtual auto get_buffer(RenderID id) const -> const IBuffer& = 0;
+
+  [[nodiscard]] virtual auto get_graphics_pipeline(RenderID id) -> IGraphicsPipeline& = 0;
+  [[nodiscard]] virtual auto get_graphics_pipeline(RenderID id) const
+      -> const IGraphicsPipeline& = 0;
 };
 
 }  // namespace glow
